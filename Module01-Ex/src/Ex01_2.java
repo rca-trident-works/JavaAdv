6ce9b157c1ed22d1be802ec03a66e23989d7bfb0
ろむねこ <nem@nem.x0.to>
2023-10-23 15:12:25 +0900

    Ex01_2

---
+++ Module01-Ex/src/Ex01_2.java
@@ -0,0 +1,8 @@
+public class Ex01_2 {
+    public static void main(String[] args) {
+        String name1 = "abc";
+        String name2 = new String("abc");
+
+        System.out.println(name1.equals(name2));
+    }
+}
