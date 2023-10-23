bb0b84c7b80efc496ab0d1451f55b3f65aa75a76
ろむねこ <nem@nem.x0.to>
2023-10-23 15:12:33 +0900

    Ex01_4

---
+++ Module01-Ex/src/Ex01_4.java
@@ -0,0 +1,6 @@
+public class Ex01_4 {
+    public static void main(String[] args) {
+        String name1 = "https://computer.trident.ac.jp/";
+        System.out.println(name1.contains("ac.jp"));
+    }
+}
