cd95e5776b5de7b024d5f7dfbd4c375193c0edf0
ろむねこ <nem@nem.x0.to>
2023-10-23 14:16:23 +0900

    ImportExample

--- Module01-01/src/ImportExample.java
+++ Module01-01/src/ImportExample.java
@@ -1,6 +1,10 @@
+import java.util.*;
+
 public class ImportExample {
     public static void main(String[] args) {
-        java.util.Random rand = new java.util.Random();
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+
         double dnum = rand.nextDouble();
         System.out.println(dnum);
         int inum1 = rand.nextInt();
