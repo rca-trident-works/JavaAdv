afa68a51db90ccee53e3634613576b6cedcbe765
ろむねこ <nem@nem.x0.to>
2023-10-25 12:22:33 +0900

    Tests

---
+++ Module01-Ex/src/aaa/Test.java
@@ -0,0 +1,13 @@
+package aaa;
+
+public class Test {
+    public String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void aaa() {
+        System.out.println(name + "さん、こんにちは");
+    }
+}
---
+++ Module01-Ex/src/bbb/TestMain.java
@@ -0,0 +1,11 @@
+package bbb;
+
+import aaa.Test;
+
+public class TestMain {
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.setName("入力する名前");
+        t.aaa();
+    }
+}
