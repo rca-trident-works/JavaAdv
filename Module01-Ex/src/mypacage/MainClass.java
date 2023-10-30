4f27df170ac24c461ce67a2bdb8b6fbee6358313
ろむねこ <nem@nem.x0.to>
2023-10-30 13:59:46 +0900

    MainClass

--- Module01-Ex/src/mypacage/MainClass.java
+++ Module01-Ex/src/mypacage/MainClass.java
@@ -6,8 +6,10 @@ public class MainClass {
     public static void main(String[] args) {
         ClassA ca = new ClassA();
         ca.publicField = 13;
+        ca.setPrivateField("aaaaa");
+        System.out.println(ca.publicField);
+        System.out.println(ca.getPrivateField());
         ca.publicMethod();
-
         ca.privateMethod();
     }
 }
