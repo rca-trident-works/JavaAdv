0e57750ff44e1bf3d657d9bda48d59e7b6c7b4d1
ろむねこ <nem@nem.x0.to>
2023-11-01 10:17:18 +0900

    ExceptionExample4

---
+++ Module02/src/ExceptionExample4.java
@@ -0,0 +1,17 @@
+public class ExceptionExample4 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 0;
+        try {
+            int c = a / b;
+            System.out.println("cの値は" + c);
+        } catch (ArithmeticException e) {
+            System.out.println("例外をキャッチしました");
+            System.out.println(e);
+            return;
+        } finally {
+            System.out.println("finally処理");
+        }
+        System.out.println("プログラムを終了します");
+    }
+}
