ad4f9059b09b8f573a0472d739610affb9f41313
ろむねこ <nem@nem.x0.to>
2023-11-01 10:18:31 +0900

    ExceptionExample6

--- Module01-Ex/src/mypacage/MainClass.java
+++ Module01-Ex/src/mypacage/MainClass.java
@@ -6,9 +6,12 @@ public class MainClass {
     public static void main(String[] args) {
         ClassA ca = new ClassA();
         ca.publicField = 13;
+
         ca.setPrivateField("aaaaa");
+
         System.out.println(ca.publicField);
         System.out.println(ca.getPrivateField());
+
         ca.publicMethod();
         ca.privateMethod();
     }
