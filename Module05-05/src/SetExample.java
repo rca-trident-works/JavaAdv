1685584d13741fa3d409417c16996d3b22dce54e
ろむねこ <nem@nem.x0.to>
2023-11-13 14:56:13 +0900

    SetExample

---
+++ Module05-05/src/SetExample.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+public class SetExample {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+
+        System.out.println(set.add("Jan"));
+        System.out.println(set.add("Feb"));
+        System.out.println(set.add("Feb"));
+        System.out.println(set.add("Mar"));
+        System.out.println(set.add("Apr"));
+
+        System.out.println(set);
+        System.out.println(set.contains("Jan"));
+        System.out.println(set.contains("Jun"));
+    }
+}
