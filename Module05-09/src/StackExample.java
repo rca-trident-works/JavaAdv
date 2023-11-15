98c25cf6eda2adf4554be50610cd4f81e2162363
ろむねこ <nem@nem.x0.to>
2023-11-15 12:48:52 +0900

    StackExample

---
+++ Module05-09/src/StackExample.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class StackExample {
+    public static void main(String[] args) {
+        LinkedList<String> stack = new LinkedList<>();
+
+        stack.push("(1)");
+        System.out.println("スタックの状態: " + stack);
+
+    }
+}
