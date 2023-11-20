58e785a92e72db898476503cfc4f29829e8e024e
ろむねこ <nem@nem.x0.to>
2023-11-20 14:17:38 +0900

    InnerClassExample2

---
+++ Module06-01/src/InnerClassExample2.java
@@ -0,0 +1,20 @@
+class Outer2 {
+    private String message = "Outerクラスのprivateインスタンス変数です";
+
+    class Inner {
+        void print() {
+            System.out.println("Innerクラスのprintメソッドが呼ばれました");
+            System.out.println(message);
+        }
+    }
+    void doSomething() {
+        Inner inner = new Inner();
+        inner.print();
+    }
+}
+public class InnerClassExample2 {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.doSomething();
+    }
+}
