ed45f90a5b5846181c56fa0340cb9bc6acd19c1f
ろむねこ <nem@nem.x0.to>
2023-11-20 14:18:09 +0900

    InnerClassExample2修正

--- Module06-01/src/InnerClassExample2.java
+++ Module06-01/src/InnerClassExample2.java
@@ -14,7 +14,7 @@ class Outer2 {
 }
 public class InnerClassExample2 {
     public static void main(String[] args) {
-        Outer outer = new Outer();
+        Outer2 outer = new Outer2();
         outer.doSomething();
     }
 }
