3521660f8e4d37af7e8a90ba71ed5fc3751604bb
ろむねこ <nem@nem.x0.to>
2023-11-22 10:24:51 +0900

    Ex6_2_4(中身入れずにコミットしちゃったので）

--- Module06-Ex2/src/Ex6_2_4.java
+++ Module06-Ex2/src/Ex6_2_4.java
@@ -1,2 +1,24 @@
+interface Operation {
+    int perform(int a, int b);
+}
+
 public class Ex6_2_4 {
+    public static void main(String[] args) {
+        Operation addition = new Operation() {
+            @Override
+            public int perform(int a, int b) {
+                return a + b;
+            }
+        };
+
+        Operation subtraction = new Operation() {
+            @Override
+            public int perform(int a, int b) {
+                return a - b;
+            }
+        };
+
+        System.out.println(addition.perform(100, 32));
+        System.out.println(subtraction.perform(100, 32));
+    }
 }
