9ae85b95b5493ebf00179ef31f6037d649ac1bde
ろむねこ <nem@nem.x0.to>
2023-11-22 11:28:30 +0900

    mainメソッドの中にうつした

--- Module06-Ex2/src/Ex6_2_2.java
+++ Module06-Ex2/src/Ex6_2_2.java
@@ -1,12 +1,11 @@
 public class Ex6_2_2 {
-    // 内部クラスとして記述
-    static class Calc implements Calculator {
-        public int calculate(int a, int b) {
-            return a + b;
-        }
-    }
-
     public static void main(String[] args) {
+        class Calc implements Calculator {
+            public int calculate(int a, int b) {
+                return a + b;
+            }
+        }
+
         Calc calculator = new Calc();
         int result = calculator.calculate(5, 7);
         System.out.println("合計: " + result);
