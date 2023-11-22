429ea2ce06bbd918c2f199be73c3a0ec509438c2
ろむねこ <nem@nem.x0.to>
2023-11-22 10:08:52 +0900

    InputExample2

---
+++ Module07-02/src/InputExample2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class InputExample2 {
+    public static void main(String[] args) {
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(in);
+        try {
+            String line = reader.readLine();
+            double val = Double.parseDouble(line);
+            System.out.println("入力された値の平方根は" + Math.sqrt(val));
+            reader.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
