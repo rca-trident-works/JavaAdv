53123fc51a92eaabd878af10ad5dfb8b05589eb1
ろむねこ <nem@nem.x0.to>
2023-11-22 11:28:33 +0900

    mainメソッドの中にうつした

--- Module06-Ex2/src/Ex6_2_3.java
+++ Module06-Ex2/src/Ex6_2_3.java
@@ -1,12 +1,13 @@
 public class Ex6_2_3 {
     // 匿名クラスとして記述
-    static Calculator calculator = new Calculator() {
-        public int calculate(int a, int b) {
-            return a + b;
-        }
-    };
 
     public static void main(String[] args) {
+        Calculator calculator = new Calculator() {
+            public int calculate(int a, int b) {
+                return a + b;
+            }
+        };
+
         int result = calculator.calculate(5, 7);
         System.out.println("合計: " + result);
     }
